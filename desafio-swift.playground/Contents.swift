bfc43c0804b297f1b697d7ee9f5cdd5dcc21add4
sandokhan <rhaydrick@gmail.com>
2023-08-21 20:59:28 +0100

    Desafio completo

--- desafio-swift.playground/Contents.swift
+++ desafio-swift.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Rhaydrick"
+// Definindo firstName (constante) com o valor inicial Steve
+let firstName = "Steve"
+
+// Definindo variável opcional
+var lastName: String? = "Jobs"
+
+print("Nome: \(firstName) \(lastName ?? "Wozniak")")
+
+// optional binding
+if let unwrappedLastName = lastName {
+    // printando após interpolação
+    print("Nome: \(firstName) \(unwrappedLastName)")
+}
